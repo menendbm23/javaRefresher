a97a007ef5406a3529130c7b76dce8374caec327
menendbm23 <menendbm@gmail.com>
2021-05-10 14:58:49 -0400

    new update

---
+++ Help/src/Main.java
@@ -0,0 +1,33 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		int x = 123; // initialization 
+		
+		System.out.println("My number is: "+x);
+		
+		
+		double y = 3.14;
+		
+		System.out.println(y);
+		
+		
+		boolean z = true;
+		
+		System.out.println(z);
+		
+		char symbol = '@';
+		
+		System.out.println(symbol);
+		
+		
+		String name = "Bryan";
+		
+		System.out.println("Hello " +name);
+		
+
+	}
+
+}
