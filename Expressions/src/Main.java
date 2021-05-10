8959cfeedb2495e67d7f6bab7b41e22c58df2f09
menendbm23 <menendbm@gmail.com>
2021-05-10 15:20:44 -0400

    initial commit

---
+++ Expressions/src/Main.java
@@ -0,0 +1,27 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// expression = operands & operators 
+		// operands = values, variables, numbers, quantity 
+		// operators = + - * / %
+		
+		double friends = 10;
+		
+		
+		friends = (double) friends / 3;
+		
+		friends++;
+		friends--;
+		
+		
+		friends = friends % 2;
+		
+		friends = friends * 34;
+				
+		System.out.println(friends);
+
+	}
+
+}
