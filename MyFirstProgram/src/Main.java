09f09c2e9d0c438ac444dc6c9ffc089944309e54
menendbm23 <menendbm@gmail.com>
2021-05-10 14:46:30 -0400

    first commit

---
+++ MyFirstProgram/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\tI love pizza\n");
+		System.out.println("It's really good!");
+		
+		// This is a comment
+		/*
+		 * This
+		 * Is
+		 * A
+		 * Comment
+		 */
+		
+		System.out.println("Bye!");
+
+	}
+
+}
+
