7c97079e505c3ea3a0118e97546683724d1d12bf
menendbm23 <menendbm@gmail.com>
2021-06-06 15:15:01 -0400

    init update

---
+++ MethodsOfString/src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String name = "Clem";
+		
+		//boolean result = name.equals("Clem");
+		//int result = name.length();
+		//char result = name.charAt(0);
+		//int result = name.indexOf("C");
+		//boolean result = name.isEmpty();
+		//String result = name.toUpperCase();
+		// result = name.toLowerCase();
+		//String result = name.trim();
+		String result = name.replace('e', 'a');
+		
+		
+		
+		
+		System.out.println(result);
+		
+
+	}
+
+}
